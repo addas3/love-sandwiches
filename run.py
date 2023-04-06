dcccc9075d122e6ca2a84722546932ac21fcf1c9
addas3 <addas3code@gmail.com>
2023-04-06 17:08:18 +0000

    Collect sales data for last 5 markets in lists of columns

--- run.py
+++ run.py
@@ -104,6 +104,22 @@ def calculate_surplus_data(sales_row):
         surplus_data.append(surplus)
     
     return surplus_data
+
+def get_last_5_entries_sales():
+    """
+    Collects collumns of data from sales worksheet, collecting
+    the last 5 entries for each sandwish and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+    
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    
+    return columns
    
 
 def main():
@@ -117,4 +133,6 @@ def main():
     update_worksheet(new_surplus_data, "surplus")
 
 print("Welcome to Love Sandwisches Data Automation")
-main()
+# main()
+
+sales_columns = get_last_5_entries_sales()
